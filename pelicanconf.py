15a2d25de91a7c5e0cc47ef81f8aa6681ed23648
wentao.xu <wentao.xu@dianping.com>
2014-01-04 20:32:32 +0800

    refresh

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,30 +2,45 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'steven'
-SITENAME = u"steven's blog"
+AUTHOR = u'Steven'
+SITENAME = u"Steven's Blog"
 SITEURL = ''
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Asia/Shanghai'
+DEFAULT_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+USE_FOLDER_AS_CATEGORY = True
+
+FILENAME_METADATA = '(?P<slug>.*)'
 
 DEFAULT_LANG = u'en'
 
+THEME = 'niu-x2'
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
+STATIC_PATHS = ['images',]
+
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('微博', 'http://weibo.com/u/1965117033'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+ARTICLE_URL = '{category}/{slug}.html'
+ARTICLE_SAVE_AS = ARTICLE_URL
+PAGE_URL = '{slug}.html'
+PAGE_SAVE_AS = PAGE_URL
+CATEGORY_URL = '{slug}/index.html'
+CATEGORY_SAVE_AS = CATEGORY_URL
+TAG_URL = 'tag/{slug}.html'
+TAG_SAVE_AS = TAG_URL
+TAGS_SAVE_AS = 'tag/index.html'
+
+JINJA_EXTENSIONS = ['jinja2.ext.ExprStmtExtension',]
